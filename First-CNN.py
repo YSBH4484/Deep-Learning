b941588ea7aee9be3899e367bdf0f2d24426f65a
YSBH4484 <L17341537118@outlook.com>
2026-05-18 16:43:48 +0800

    new model

--- First-CNN.py
+++ First-CNN.py
@@ -3,3 +3,58 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import torch
+import torchvision
+import torch.utils.data as data
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+from torchvision import datasets, transforms
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(device)
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.322,), (0.867,))
+])
+
+train_dataset = datasets.MNIST(root='./data', train=True, transform=transform, download=True)
+test_dataset = datasets.MNIST(root = './data', train=False, transform=transform, download=True)
+train_loader = data.DataLoader(train_dataset, batch_size=64, shuffle=True)
+test_loader = data.DataLoader(test_dataset, batch_size=1000, shuffle=False)
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(784, 128),
+            nn.ReLU(),
+            nn.Linear(128, 30),
+            nn.ReLU(),
+            nn.Linear(30, 20),
+            nn.ReLU(),
+            nn.Linear(20, 10),
+        )
+
+    def forward(self, x):
+        return self.fc(x)
+
+model = Net().to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr = 0.01)
+
+for epoch in range(10):
+    model.train()
+    total_loss = 0.0
+    for images, labels in train_loader:
+        images, labels = images.to(device), labels.to(device)
+        optimizer.zero_grad()
+        output = model(images)
+        loss = criterion(output, labels)
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item()
+    print(f"Epoch : {epoch + 1}, loss is {total_loss/len(train_loader):.3f}\n")
+
+torch.save(model.state_dict(), "model.pth")
